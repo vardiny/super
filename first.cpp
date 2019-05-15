cc04139626400ad5b503e191dba2b8883a03104d
vardiny <vardiny@list.ru>
2019-05-15 12:58:41 +0300

    Написала Hello world

---
+++ first.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib> // для system
+using namespace std;
+
+int main() 
+{ 
+    cout << "Hello, world!" << endl;
+    int x;
+    cin >> x; 
+    // system("pause"); // Только для тех, у кого MS Visual Studio
+    return 0; 
+}
