70231770f1cf6c1b54dde8315a78c889423a51ea
vardiny <vardiny@list.ru>
2019-05-22 13:32:01 +0300

    Посчитала сколько гласных в словах

---
+++ julii.cpp
@@ -0,0 +1,39 @@
+#include <string.h>
+#include <cstdlib> // для system
+using namespace std;
+
+int main()
+{
+//    char s[6] = {'a','e','o','u','i'};
+    int a = 0, e = 0, o = 0, u = 0, i = 0, y = 0;
+    string b;
+    getline(cin,b);
+	for (int z = 0; z < b.length(); z++) {
+		if (b[z] =='a') {
+			a = a + 1;
+		}
+		if(b[z] == 'e') {
+			e = e + 1;
+		}
+		if(b[z] == 'i') {
+			i = i + 1;
+		}
+		if (b[z] == 'o') {
+			o = o + 1;
+		}
+		if (b[z] == 'u') {
+			u = u + 1;
+		}
+		if (b [z] == 'y') {
+			y = y + 1;
+		}
+	}
+    cout << "a = " << a << endl;
+    cout << "e = " << e << endl;
+    cout << "o = " << o << endl;
+    cout << "u = " << u << endl;
+    cout << "i = " << i << endl;
+
+    // system("pause"); // Только для тех, у кого MS Visual Studio
+    return 0;
+}
