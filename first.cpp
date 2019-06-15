2b2ed13ca2856aa8c25582927adae0d8d7cc57cb
vardiny <vardiny@list.ru>
2019-06-15 16:00:31 +0300

    Создала функцию из practic#3

--- first.cpp
+++ first.cpp
@@ -2,8 +2,9 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
- 
+
  using namespace std;
+ 
  int main()
  {
      string mas;
@@ -15,14 +16,11 @@
         if(mas[i]=='Y'){mas[i]='y';};
         if(mas[i]=='U'){mas[i]='u';};
         if(mas[i]=='O'){mas[i]='o';};
-        if(mas[i]=='E'){mas[i]='e';};    
-
+        if(mas[i]=='E'){mas[i]='e';};   
     }
-     for (int i = 0; i < mas.length(); i++)
+    for (int i = 0; i < mas.length(); i++)
 	{
-    cout<<mas[i];
+        cout<<mas[i];
     }
-
-
-return 0;
+    return 0;
  }
