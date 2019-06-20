4354f13e97a2b67b906423fb109e6d5fabacd134
vardiny <vardiny@list.ru>
2019-06-20 14:35:29 +0300

    Сделала функцию из practic#1

---
+++ practic#2.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <iostream>
+#include <cstdlib> // для system
+using namespace std;
+void kolgl(string bus)
+{
+    int a = 0, e = 0, o = 0, u = 0, i = 0, y = 0;
+    for (int z = 0; z < bus.length(); z++) 
+    {
+		if (bus[z] =='a') {
+			a = a + 1;
+		}
+		if(bus[z] == 'e') {
+			e = e + 1;
+		}
+		if(bus[z] == 'i') {
+			i = i + 1;
+		}
+		if (bus[z] == 'o') {
+			o = o + 1;
+		}
+		if (bus[z] == 'u') {
+			u = u + 1;
+		}
+		if (bus[z] == 'y') {
+			y = y + 1;
+		}
+	}
+    cout << "a = " << a << endl;
+    cout << "e = " << e << endl;
+    cout << "o = " << o << endl;
+    cout << "u = " << u << endl;
+    cout << "i = " << i << endl;
+	
+
+}
+
+int main()
+{
+    int a = 0, e = 0, o = 0, u = 0, i = 0, y = 0;
+    string bus;
+    getline(cin,bus);
+	kolgl(bus);
+    // system("pause"); // Только для тех, у кого MS Visual Studio
+    return 0;
+}
