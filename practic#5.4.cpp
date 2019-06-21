f295d47400cad539908767f9642bc59b7c43b769
vardiny <vardiny@list.ru>
2019-06-22 01:26:58 +0300

    Тест prsctic#4

---
+++ practic#5.4.cpp
@@ -0,0 +1,32 @@
+//practic#4
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+ using namespace std;
+ string funct(string mas)
+ {
+     for (int i = 0; i < mas.length(); i++)
+	{
+        if(mas[i]=='A'){mas[i]='a';};
+        if(mas[i]=='I'){mas[i]='i';};
+        if(mas[i]=='Y'){mas[i]='y';};
+        if(mas[i]=='U'){mas[i]='u';};
+        if(mas[i]=='O'){mas[i]='o';};
+        if(mas[i]=='E'){mas[i]='e';};   
+    }
+    
+    return mas;
+   
+ }
+ int main()
+ {
+     string sup = "HELLO WORLD";
+     sup=funct(sup);
+     cout<<sup<<endl;
+     if (sup == "HeLLo WoRLD"){cout<<"Right";}
+     else {cout<<"Wrong";}
+
+    return 0;
+ }
+
