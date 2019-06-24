16357b9121a874fc75b70253ee6147eb6bf99135
vardiny <vardiny@list.ru>
2019-06-24 23:36:57 +0300

    Сделала функцию из practic#1

--- practic#2.cpp
+++ practic#2.cpp
@@ -1,46 +1,25 @@
-#include <string>
 #include <iostream>
+#include <string>
 #include <cstdlib> // для system
 using namespace std;
-void kolgl(string bus)
+void kolgl(string bus, int &a, int &e, int &o, int &u, int &i, int &y)
 {
-    int a = 0, e = 0, o = 0, u = 0, i = 0, y = 0;
-    for (int z = 0; z < bus.length(); z++) 
-    {
-		if (bus[z] =='a') {
-			a = a + 1;
-		}
-		if(bus[z] == 'e') {
-			e = e + 1;
-		}
-		if(bus[z] == 'i') {
-			i = i + 1;
-		}
-		if (bus[z] == 'o') {
-			o = o + 1;
-		}
-		if (bus[z] == 'u') {
-			u = u + 1;
-		}
-		if (bus[z] == 'y') {
-			y = y + 1;
-		}
-	}
-    cout << "a = " << a << endl;
-    cout << "e = " << e << endl;
-    cout << "o = " << o << endl;
-    cout << "u = " << u << endl;
-    cout << "i = " << i << endl;
-	
+  for (int z = 0; z < bus.length(); z++) {
+		if (bus[z] == 'a') {		a = a + 1;		}	
+    	if (bus[z] == 'e') {		e = e + 1;		}	
+    	if (bus[z] == 'i') {	 	i = i + 1;	  	}	
+    	if (bus[z] == 'o') {		o = o + 1;		}	
+    	if (bus[z] == 'u') {		u = u + 1;		}	
+    	if (bus[z] == 'y') {		y = y + 1;		}
+	} 
 
-}
-
-int main()
-{
-    int a = 0, e = 0, o = 0, u = 0, i = 0, y = 0;
-    string bus;
-    getline(cin,bus);
-	kolgl(bus);
-    // system("pause"); // Только для тех, у кого MS Visual Studio
-    return 0;
+} 
+int main(){ 
+int a = 0, e = 0, o = 0, u = 0, i = 0, y = 0;
+ string bus; 
+getline(cin,bus);
+kolgl(bus,a,e,o,u,i,y);
+ // system("pause"); // Только для тех, у кого MS Visual Studio
+ cout << "a = " << a << endl; cout << "e = " << e << endl; cout << "o = " << o << endl; cout << "u = " << u << endl; cout << "i = " << i << endl;
+ return 0;
 }
