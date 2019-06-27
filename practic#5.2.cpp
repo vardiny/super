e2f117226707bcfe4d93d85a279c85291a33dd06
vardiny <vardiny@list.ru>
2019-06-27 08:45:29 +0300

    исправила отступы

--- practic#5.2.cpp
+++ practic#5.2.cpp
@@ -15,24 +15,26 @@ void kolgl(string bus, int &a, int &e, int &o, int &u, int &i, int &y)
 
 } 
 int main(){ 
-int a = 0, e = 0, o = 0, u = 0, i = 0, y = 0;
- string bus; 
-bus = "Hello world";
-kolgl(bus,a,e,o,u,i,y);
-if ((a==0)&&(e==1)&&(o==2)&&(u==0)&&(i==0)&&(y==0)){cout<<"Right"<<endl;}
-else {cout<<"Wrong"<<endl;};
-bus = "123456 Polly function";
-a = 0, e = 0, o = 0, u = 0, i = 0, y = 0;
-kolgl(bus,a,e,o,u,i,y);
-if ((a==0)&&(e==0)&&(o==2)&&(u==1)&&(i==1)&&(y==1)){cout<<"Right"<<endl;}
-else {cout<<"Wrong"<<endl;};
-bus = "";
-a = 0, e = 0, o = 0, u = 0, i = 0, y = 0;
-kolgl(bus,a,e,o,u,i,y);
-if ((a==0)&&(e==0)&&(o==0)&&(u==0)&&(i==0)&&(y==0)){cout<<"Right"<<endl;}
-else {cout<<"Wrong"<<endl;};
+	int a = 0, e = 0, o = 0, u = 0, i = 0, y = 0;
+	string bus; 
+	bus = "Hello world";
+	kolgl(bus,a,e,o,u,i,y);
+	if ((a==0)&&(e==1)&&(o==2)&&(u==0)&&(i==0)&&(y==0)){cout<<"Right"<<endl;}
+	else {cout<<"Wrong"<<endl;};
 
- // system("pause"); // Только для тех, у кого MS Visual Studio
- //cout << "a = " << a << endl; cout << "e = " << e << endl; cout << "o = " << o << endl; cout << "u = " << u << endl; cout << "i = " << i << endl; cout << "y = " << y << endl;
- return 0;
+	bus = "123456 Polly function";
+	a = 0, e = 0, o = 0, u = 0, i = 0, y = 0;
+	kolgl(bus,a,e,o,u,i,y);
+	if ((a==0)&&(e==0)&&(o==2)&&(u==1)&&(i==1)&&(y==1)){cout<<"Right"<<endl;}
+	else {cout<<"Wrong"<<endl;};
+	
+	bus = "";
+	a = 0, e = 0, o = 0, u = 0, i = 0, y = 0;
+	kolgl(bus,a,e,o,u,i,y);
+	if ((a==0)&&(e==0)&&(o==0)&&(u==0)&&(i==0)&&(y==0)){cout<<"Right"<<endl;}
+	else {cout<<"Wrong"<<endl;};
+
+ 	// system("pause"); // Только для тех, у кого MS Visual Studio
+ 	//cout << "a = " << a << endl; cout << "e = " << e << endl; cout << "o = " << o << endl; cout << "u = " << u << endl; cout << "i = " << i << endl; cout << "y = " << y << endl;
+ 	return 0;
 }
